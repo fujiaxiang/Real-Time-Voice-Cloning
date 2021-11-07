8f6078014e771ad77da22e026d4549e6a78d4b74
Jiaxiang <fujiaxiang1995@gmail.com>
2021-11-07 10:06:44 +0800

    Added emtional synthesizer model based on Tacotron

---
+++ synthesizer/models/emo_models.py
@@ -0,0 +1,18 @@
+import torch
+
+from synthesizer.models.tacotron import Tacotron
+
+
+class MultispeakerEmotionalSynthesizer(Tacotron):
+    def __init__(self, embed_dims, num_chars, encoder_dims, decoder_dims, n_mels,
+                 fft_bins, postnet_dims, encoder_K, lstm_dims, postnet_K, num_highways,
+                 dropout, stop_threshold, speaker_embedding_size, emotion_embedding_size):
+        super().__init__(embed_dims, num_chars, encoder_dims, decoder_dims, n_mels,
+                 fft_bins, postnet_dims, encoder_K, lstm_dims, postnet_K, num_highways,
+                 dropout, stop_threshold, speaker_embedding_size + emotion_embedding_size)
+
+    def forward(self, x, m, speaker_embedding, emotion_embedding=None):
+        # Concatenating the speaker embedding and emotion_embedding, and reusing existing
+        # model architecture
+        embed = torch.cat([speaker_embedding, emotion_embedding], dim=1)
+        return super().forward(x, m, embed)
