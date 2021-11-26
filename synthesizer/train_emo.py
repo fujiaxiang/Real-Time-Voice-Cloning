6911d417743e4685fff683839f788188e027e355
Jiaxiang <fujiaxiang1994@gmail.com>
2021-11-26 23:20:04 +0800

    updated synthesizer training schedule for transfer learning

--- synthesizer/train_emo.py
+++ synthesizer/train_emo.py
@@ -317,9 +317,9 @@ if __name__ == "__main__":
         data_paths=data_paths,
         models_dir=Path("synthesizer/saved_models/"),
         training_schedule=[  # (r, lr, step, batch_size, batch_split)
-            (2,  1e-4, 160_000,  12, 2),
-            (2,  3e-5, 320_000,  12, 2),
-            (2,  1e-5, 640_000,  12, 2)
+            (2,  1e-4, 20_000,  12, 2),
+            (2,  3e-5, 40_000,  12, 2),
+            (2,  1e-5, 80_000,  12, 2)
         ],
         eval_every=500,
         save_every=5000,
